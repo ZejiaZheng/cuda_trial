9164cd641951ac5467117e7676b2b4e9286cc5be
buttonzzj <buttonzhengze@hotmail.com>
2016-05-16 20:51:21 -0400

    added gpu serial transpose

--- mat_transpose.cu
+++ mat_transpose.cu
@@ -12,7 +12,22 @@ void fill_matrix(float* mat){
     }
 }
 
+void compare_matrices(float* mat1, float* mat2){
+    for (int i = 0; i < N*N; i++){
+        if(mat1[i] != mat2[i]) return 0;
+    }
+    return 1;
+}
+
 void transpose_cpu(float* mat, float* mat_new){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j< N; j++){
+            mat_new[j*N + i] = mat[i*N + j];
+        }
+    }
+}
+
+void transpose_serial(float* mat, float* mat_new){
     for (int i = 0; i < N; i++){
         for (int j = 0; j< N; j++){
             mat_new[j*N + i] = mat[i*N + j];
@@ -25,14 +40,31 @@ int main(int argc, char const *argv[])
     size_t mat_size = N * N * sizeof(float);
     float* mat = (float *) malloc(mat_size);
     float* mat_new = (float *) malloc(mat_size);
+    float* mat_gold = (float *) malloc(mat_size);
 
     fill_matrix(mat);
 
     clock_t start = clock(), diff;
-    transpose_cpu(mat, mat_new);
+    transpose_cpu(mat, mat_gold);
     diff = clock() - start;
     int msec = diff * 1000 / CLOCKS_PER_SEC;
-    printf("CPU Transpose time taken %d seconds, %d milliseconds", msec/1000, msec%1000);
+    printf("CPU Transpose time taken %d seconds, %d milliseconds \n", msec/1000, msec%1000);
+
+
+    float *d_in, *d_out;
+
+    cudaMalloc(&d_in, mat_size);
+    cudaMalloc(&d_out, mat_size);
+    cudaMemcpy(d_in, mat, mat_size, cudaMemcpyHostToDevice);
+
+    GpuTimer timer;
+    timer.Start();
+    transpose_serial<<<1,1>>>(d_in, d_out);
+    timer.Stop();
+
+    cudaMemcpy(d_out, mat_new, mat_size, cudaMemcpyDeviceToHost);
+
+    printf("transpose_parallel_per_row: %g ms.\nVerifying transpose...%s\n", timer.Elapsed(), compare_matrices(mat_new, mat_gold) ? "Failed" : "Success");
 
     
 
@@ -48,13 +80,13 @@ int main(int argc, char const *argv[])
     printf("transposed_mat: \n");
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            printf("%3.1f ", mat_new[i*N + j]);
+            printf("%3.1f ", mat_gold[i*N + j]);
         }
         printf("\n");
     }*/
 
     free(mat);
-    free(mat_new);
+    free(mat_gold);
 
     return 0;
 }
