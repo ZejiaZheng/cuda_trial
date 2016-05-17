f9e1bd56590d9da579237853937228e914553c7d
buttonzzj <buttonzhengze@hotmail.com>
2016-05-16 20:32:39 -0400

    added cpu transpose

--- mat_transpose.cu
+++ mat_transpose.cu
@@ -11,12 +11,23 @@ void fill_matrix(float* mat){
     }
 }
 
+void transpose_cpu(float* mat, float* mat_new){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j< N; j++){
+            mat_new[j*N + i] = mat[i*N + j];
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     size_t mat_size = N * N * sizeof(float);
     float* mat = (float *) malloc(mat_size);
+    float* mat_new = (float *) malloc(mat_size);
 
     fill_matrix(mat);
+
+    transposed_cpu(mat, mat_new);
 
     // show orignal mat, for debug purposes
     printf("original_mat: \n");
@@ -26,6 +37,17 @@ int main(int argc, char const *argv[])
         }
         printf("\n");
     }
+
+    printf("transposed_mat: \n");
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            printf("%3.1f ", mat_new[i*N + j]);
+        }
+        printf("\n");
+    }
+
+    free(mat);
+    free(mat_new);
 
     return 0;
 }
