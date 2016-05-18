bd0af0eb5bf93e9c67328abc0d6da8756dfa788b
buttonzzj <buttonzhengze@hotmail.com>
2016-05-17 23:08:47 -0400

    added histogram using thrust

---
+++ thrust_hist.cu
@@ -0,0 +1,120 @@
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <thrust/random.h>
+#include <thrust/inner_product.h>
+#include <thrust/binary_search.h>
+#include <thrust/adjacent_difference.h>
+#include <thrust/iterator/constant_iterator.h>
+#include <thrust/iterator/counting_iterator.h>
+
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <time.h>
+
+
+// simple routine to print contents of a vector
+template <typename Vector>
+void print_vector(const std::string& name, const Vector& v)
+{
+  typedef typename Vector::value_type T;
+  std::cout << "  " << std::setw(20) << name << "  ";
+  thrust::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, " "));
+  std::cout << std::endl;
+}
+
+// dense histogram using binary search
+template <typename Vector1, 
+          typename Vector2>
+void dense_histogram(const Vector1& input,
+                           Vector2& histogram)
+{
+  typedef typename Vector1::value_type ValueType; // input value type
+  typedef typename Vector2::value_type IndexType; // histogram index type
+
+  // copy input data (could be skipped if input is allowed to be modified)
+  thrust::device_vector<ValueType> data(input);
+    
+  // print the initial data
+  //print_vector("initial data", data);
+
+  // sort data to bring equal elements together
+  thrust::sort(data.begin(), data.end());
+  
+  // print the sorted data
+  //print_vector("sorted data", data);
+
+  // number of histogram bins is equal to the maximum value plus one
+  IndexType num_bins = data.back() + 1;
+
+  // resize histogram storage
+  histogram.resize(num_bins);
+  
+  // find the end of each bin of values
+  thrust::counting_iterator<IndexType> search_begin(0);
+  thrust::upper_bound(data.begin(), data.end(),
+                      search_begin, search_begin + num_bins,
+                      histogram.begin());
+  
+  // print the cumulative histogram
+  //print_vector("cumulative histogram", histogram);
+
+  // compute the histogram by taking differences of the cumulative histogram
+  thrust::adjacent_difference(histogram.begin(), histogram.end(),
+                              histogram.begin());
+
+  // print the histogram
+  // print_vector("histogram", histogram);
+}
+
+int main(void)
+{
+  const int numBins = 1024;
+  const int N = numBins*10000;
+
+  // generate random data on the host
+  thrust::host_vector<int> input(N);
+
+  unsigned int mean = rand() % 100 + 462;
+
+  //Output mean so that grading can happen with the same inputs
+  std::cout << mean << std::endl;
+
+  thrust::minstd_rand rng;
+
+  thrust::random::normal_distribution<float> normalDist((float)mean, stddev);
+
+  // Generate the random values
+  for (size_t i = 0; i < N; ++i) {
+    input[i] = std::min((unsigned int) std::max((int)normalDist(rng), 0), numBins - 1);
+  }
+
+  // demonstrate dense histogram method
+  GpuTimer timer;
+  timer.Start();
+  clock_t start, end;
+  start = clock();
+  {
+    std::cout << "Dense Histogram" << std::endl;
+    thrust::device_vector<int> histogram;
+    dense_histogram(input, histogram);
+  }
+  end = clock();
+  timer.Stop();
+  time_taken = (double)(end-start) * 1000.0/CLOCKS_PER_SEC;
+  printf("time taken: %3.2f msecs\n", time_taken);
+
+  
+  int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
+
+  // Note: 
+  // A dense histogram can be converted to a sparse histogram
+  // using stream compaction (i.e. thrust::copy_if).
+  // A sparse histogram can be expanded into a dense histogram
+  // by initializing the dense histogram to zero (with thrust::fill)
+  // and then scattering the histogram counts (with thrust::scatter).
+
+  return 0;
+}
